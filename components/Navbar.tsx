02fa64487023eaf98e6e2663441b1410263deb4a
HossinHabil <husseinhabil92@gmail.com>
2024-02-05 00:57:55 +0200

    fix navbar bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import Link from "next/link";
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 import Icon from "./Icon";
 import { NavBarLinks } from "@/data/prjectsData";
-import { useActivePath } from "@/lib/helpers";
 
 const Navbar = () => {
-  const checkActivePath = useActivePath();
   return (
     <nav className="fixed bottom-4 w-full text-center z-50">
       <ul className="flex items-center justify-evenly gap-5 w-80 bg-gradient-to-r from-[#67e9ff] to-[#62d7eb] mx-auto p-4 rounded-2xl">
@@ -15,7 +11,7 @@ const Navbar = () => {
           <Link
             href={link.link}
             key={link.id}
-            className={`text-lg font-semibold text-white dark:text-white ${checkActivePath(link.link) ? 'bg-slate-500' : ''}`}
+            className="text-lg font-semibold text-white dark:text-white"
             scroll={true}
           >
             <Icon
