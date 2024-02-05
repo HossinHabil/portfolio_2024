f6ac6dc4ad70ca4a28ed0ec7b61cd81606dfe542
HossinHabil <husseinhabil92@gmail.com>
2024-02-05 10:56:26 +0200

    update the CV

--- components/ResumeButton.tsx
+++ components/ResumeButton.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button'
 const ResumeButton = () => {
   return (
     <Button className="bg-[#67e9ff] hover:bg-[#62d7eb] transition-all dark:bg-accent dark:hover:bg-slate-700 text-white border-none rounded-md mx-auto">
-      <a href="/Hossin-Habil-CV.pdf" rel="noreferrer" download>
+      <a href="/Hossin_Habil_CV.pdf" rel="noreferrer" download>
         Resume
       </a>
     </Button>
