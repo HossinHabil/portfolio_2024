8ac7135e2ad549f68b2120f7a58e6c9e6033bb7a
HossinHabil <husseinhabil92@gmail.com>
2024-12-11 19:16:22 +0200

    update the gitInTouch font-size

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -7,7 +7,7 @@ const ContactSection = () => {
       className="flex flex-col lg:flex-row gap-8 items-center justify-between text-center w-full md:px-8 pt-20 pb-24"
     >
       <div className="flex flex-col space-y-5">
-        <h1 className="text-4xl md:text-4xl lg:text-6xl font-extrabold text-center">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold text-center">
           Get In Touch
         </h1>
         <p className="text-3xl md:text-4xl">Let&apos;s Talk 👋</p>
